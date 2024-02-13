f112e78d5f4a783cd7c085d5997365457a4d1559
Nandan Reddy <nandan.reddy1133@gmail.com>
2024-02-13 23:49:32 +0530

    Change icons from two tone to rounded

--- src/shared/Icon/AppIcon.tsx
+++ src/shared/Icon/AppIcon.tsx
@@ -2,11 +2,11 @@ import { AppIconOptions } from './AppIconOptions';
 import { AppIcons } from './AppIcons';
 
 // PLEASE COMMENT THE UNUSED RESOLVER TO AVOID BLOATING THE BUNDLE
-// import resolveRoundedMuiIcon from './resolveRoundedAppIcon';
-import resolveTwoToneMuiIcon from './resolveTwoToneAppIcon';
+import resolveRoundedMuiIcon from './resolveRoundedAppIcon';
+// import resolveTwoToneMuiIcon from './resolveTwoToneAppIcon';
 
-// const resolveMuiIcon = resolveRoundedMuiIcon;
-const resolveMuiIcon = resolveTwoToneMuiIcon;
+const resolveMuiIcon = resolveRoundedMuiIcon;
+// const resolveMuiIcon = resolveTwoToneMuiIcon;
 
 const defaultOptions: AppIconOptions = {
 	fontSize: 'large',
