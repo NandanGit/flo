14dd713f4bebec9d43eefac631d4e5fc360e0acb
Nandan Reddy <nandan.reddy1133@gmail.com>
2024-02-19 15:01:02 +0530

    Add useCallback and useEffect to useUserState hook

--- src/redux/hooks/useUserState.ts
+++ src/redux/hooks/useUserState.ts
@@ -1,3 +1,4 @@
+import { useCallback, useEffect } from 'react';
 import { fetchUserProfile } from '../slices/userSlice';
 import { useAppDispatch, useAppSelector } from './customReduxHooks';
 
@@ -6,9 +7,19 @@ const useUserState = () => {
 	const userStatus = useAppSelector((state) => state.user.status);
 	const userProfile = useAppSelector((state) => state.user.profile);
 
-	const loadUserProfile = () => {
+	const loadUserProfile = useCallback(() => {
+		console.debug('Loading user profile');
 		dispatch(fetchUserProfile());
-	};
+		return () => {
+			console.debug('Cleaning up user profile');
+		};
+	}, [dispatch]);
+
+	useEffect(() => {
+		if (userStatus === 'idle') {
+			loadUserProfile();
+		}
+	}, [userStatus, dispatch, loadUserProfile]);
 
 	return {
 		loadUserProfile,
