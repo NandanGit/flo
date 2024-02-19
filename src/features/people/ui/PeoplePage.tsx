e5415c943e864e71992b854f30fa8c158e010fef
Nandan Reddy <nandan.reddy1133@gmail.com>
2024-02-19 23:37:21 +0530

    Refactor PeoplePage component to use ListItemText

--- src/features/people/ui/PeoplePage.tsx
+++ src/features/people/ui/PeoplePage.tsx
@@ -1,4 +1,4 @@
-import { List, ListItem } from '@mui/material';
+import { List, ListItem, ListItemText } from '@mui/material';
 import { AppPage } from '../../../shared/pages/AppPage';
 import { usePeoplePageView } from '../PeoplePageView';
 import RefreshHeader from '../../../shared/components/RefreshHeader/RefreshHeader';
@@ -7,7 +7,7 @@ const PeoplePage: React.FC = () => {
 	const { people, peopleAreLoading, peopleStatus, loadPeople } =
 		usePeoplePageView();
 	return (
-		<AppPage title='People' useStaticGlassBackground={true}>
+		<AppPage title='People'>
 			<RefreshHeader
 				title='People'
 				dataStatus={peopleStatus}
@@ -18,15 +18,8 @@ const PeoplePage: React.FC = () => {
 			) : (
 				<List>
 					{people.map((person) => (
-						<ListItem
-							key={person.id}
-							style={
-								{
-									// height: '5rem',
-								}
-							}
-						>
-							{person.name}
+						<ListItem key={person.id}>
+							<ListItemText primary={person.name} />
 						</ListItem>
 					))}
 				</List>
