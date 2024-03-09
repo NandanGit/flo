4c25f7f849926d39c7ef3317ad20ada72f59f89d
Nandan Reddy <nandan.reddy1133@gmail.com>
2024-03-10 00:10:45 +0530

    Refactor default settings section constant

--- src/features/user/settings/ui/SettingsSectionContentView.ts
+++ src/features/user/settings/ui/SettingsSectionContentView.ts
@@ -1,9 +1,10 @@
 import { useParams } from 'react-router-dom';
 import { SettingsSection } from '../model/SettingsSection';
+import { AppConstants } from '../../../../shared/constants/AppConstants';
 
 const useSettingsSectionContentView = () => {
 	const { section: sectionUrl } = useParams();
-	let selected = SettingsSection.GENERAL;
+	let selected = AppConstants.defaultSettingsSection;
 	if (sectionUrl) {
 		if (
 			Object.values(SettingsSection).includes(sectionUrl as SettingsSection)
