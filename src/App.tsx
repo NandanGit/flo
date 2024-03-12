872273841525f56c7dffd4c3143e39b0409ffd50
Nandan Reddy <nandan.reddy1133@gmail.com>
2024-03-13 00:27:55 +0530

    Add import statement for s2cTransactionSchema and log parsing result

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import { s2cTransactionSchema } from '@flo.app/schemas';
 import AppRouter from './common/navigation/AppRouter';
-
+console.log('Parsing result:', s2cTransactionSchema.safeParse({}));
 function App() {
 	return <AppRouter />;
 }
