f3f31afde64d5e2a97ddfb780ebbdd28eeacaa36
Nandan Reddy <nandan.reddy1133@gmail.com>
2024-03-12 23:57:44 +0530

    Add rollup to the schemas packaging

---
+++ packages/schemas/rollup.config.js
@@ -0,0 +1,30 @@
+import typescript from 'rollup-plugin-typescript2';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+	input: 'src/index.ts',
+	output: [
+		{
+			file: 'dist/index.js',
+			format: 'cjs',
+		},
+		{
+			file: 'dist/index.es.js',
+			format: 'esm',
+		},
+	],
+	plugins: [
+		typescript({
+			useTsconfigDeclarationDir: true,
+		}),
+		resolve(),
+		commonjs(),
+		terser(),
+		// typescript({
+		// 	tsconfig: './tsconfig.rollup.json',
+		// }),
+	],
+	// plugins: [],
+};
