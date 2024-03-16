a90207a758c4c53a37915959bf31f4e8fe7bcbaf
Nandan Reddy <nandan.reddy1133@gmail.com>
2024-03-16 19:54:51 +0530

    Update color list in SharedSchemaConstants

--- packages/constants/src/schema/shared.ts
+++ packages/constants/src/schema/shared.ts
@@ -10,15 +10,29 @@ export class SharedSchemaConstants {
 	] as const;
 
 	public static readonly COLORS = [
-		// 8 simple non intrusive colors
 		'PURPLE', // #9370DB,
-		'SEA_GREEN', // #3CB371,
-		'TURQUOISE', // #48D1CC,
-		'GOLD', // #FFAF1A,
 		'TOMATO', // #FF6347,
+		// Green
 		'PISTACHIO', // #93C572,
+		'SEA_GREEN', // #3CB371,
+		// Blue
+		'TURQUOISE', // #48D1CC,
+		// Pink
 		'PINK', // #FF9999,
 		'HOT_PINK', // #FF69B4,
+		'PALE_VIOLET_RED', // #DB7093,
+		// Yellow
+		'GOLD', // #FFDB58,
+		// Others
 		// 'VIOLET_RED', // #C71585,
+		'SKY_BLUE', // #87CEEB
+		'CORAL', // #FF7F50
+		// 'Lavender', // #E6E6FA
+		'PEACH', // #FFDAB9
+		// 'Teal', // #008080
+		'ROSE_GOLD', // #B76E79
+		// 'Terracotta', // #E2725B
+		// 'Olive Green', // #808000
+		'LILAC', // #C8A2C8
 	] as const;
 }
