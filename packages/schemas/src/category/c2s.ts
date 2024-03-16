59253397604e8f8c4a7396b4829ee72d5bca89e0
Nandan Reddy <nandan.reddy1133@gmail.com>
2024-03-16 23:42:14 +0530

    Add category schemas

---
+++ packages/schemas/src/category/c2s.ts
@@ -0,0 +1,7 @@
+import c2sCategorySchemaRaw from './c2sRaw';
+const c2sCategorySchema = c2sCategorySchemaRaw;
+// .transform((data) => {
+// 	return data;
+// })
+
+export default c2sCategorySchema;
