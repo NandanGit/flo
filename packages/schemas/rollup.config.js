52e8357445f987ddcff3406d0853db19f0e569cb
Nandan Reddy <nandan.reddy1133@gmail.com>
2024-03-16 19:05:50 +0530

    Add rollup-plugin-peer-deps-external to rollup config

--- packages/schemas/rollup.config.js
+++ packages/schemas/rollup.config.js
@@ -2,6 +2,7 @@ import typescript from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 export default {
 	input: 'src/index.ts',
@@ -16,15 +17,12 @@ export default {
 		},
 	],
 	plugins: [
+		peerDepsExternal(),
 		typescript({
 			useTsconfigDeclarationDir: true,
 		}),
 		resolve(),
 		commonjs(),
 		terser(),
-		// typescript({
-		// 	tsconfig: './tsconfig.rollup.json',
-		// }),
 	],
-	// plugins: [],
 };
