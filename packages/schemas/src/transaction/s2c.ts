4f33ab3b6528dc270da22c98599cb9904b6a7aba
Nandan Reddy <nandan.reddy1133@gmail.com>
2024-03-18 00:41:18 +0530

    Refactor isSettled condition in s2cTransactionSchema

--- packages/schemas/src/transaction/s2c.ts
+++ packages/schemas/src/transaction/s2c.ts
@@ -5,7 +5,8 @@ import s2cTransactionSchemaRaw from './s2cRaw';
 // This is only for the purpose of transforming the data. It is not used for refine methods
 const s2cTransactionSchema = s2cTransactionSchemaRaw.transform((data) => {
 	const { split } = data;
-	const isSettled = split?.splits.every((split) => split.debtStatus === 'PAID');
+	// If none of them are pending, then it is settled
+	const isSettled = split?.splits.every(({ status }) => status !== 'PENDING');
 
 	return {
 		...data,
