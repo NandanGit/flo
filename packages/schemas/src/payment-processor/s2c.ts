f55a954b39f470689c344cc14d2f3fa892568b4f
Nandan Reddy <nandan.reddy1133@gmail.com>
2024-03-18 21:22:03 +0530

    Update payment processor schemas

---
+++ packages/schemas/src/payment-processor/s2c.ts
@@ -0,0 +1,6 @@
+import s2cPaymentProcessorSchemaRaw from './s2cRaw';
+
+const s2cPaymentProcessorSchema = s2cPaymentProcessorSchemaRaw;
+// .transform((data) => {
+
+export default s2cPaymentProcessorSchema;
