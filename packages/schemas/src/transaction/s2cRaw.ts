1691a7f496b92a4aef05d8a2387b09fe3a9df4e4
Nandan Reddy <nandan.reddy1133@gmail.com>
2024-03-20 00:21:28 +0530

    Rearrange the properties in the s2cTransactionSchemaRaw
    Useful when developing the user interface for the create transaction page.

--- packages/schemas/src/transaction/s2cRaw.ts
+++ packages/schemas/src/transaction/s2cRaw.ts
@@ -8,6 +8,7 @@ import {
 	merchantIdSchema,
 	personIdSchema,
 	paymentProcessorIdSchema,
+	productIdSchema,
 } from '../shared/zodId';
 import { FloConstants } from '@flo.app/constants';
 import { zodStringSchema } from '../shared/zodString';
@@ -30,36 +31,20 @@ const s2cTransactionSchemaRaw = z.object({
 	// 	maxLength: TR.title.MAX_LENGTH,
 	// }),
 
-	note: zodStringSchema({
-		fieldName: 'Transaction description',
-		maxLength: TR.description.MAX_LENGTH,
-	}).optional(),
-
-	amount: zodNumberSchema({
-		fieldName: 'Transaction amount',
-		min: TR.amount.MIN,
-		max: TR.amount.MAX,
-	}),
-
+	// This will appear in the same order as the fields in the form (hopefully)
 	type: zodEnumSchema(TR.TYPES, {
 		fieldName: 'Transaction type',
 	}),
 
-	status: zodEnumSchema(TR.STATUSES, {
-		fieldName: 'Transaction status',
-	}),
+	/**
+	 * if type is 'EXPENSE' show the product field (still optional)
+	 */
+	product: productIdSchema.optional(),
 
-	mode: zodEnumSchema(TR.MODES, {
-		fieldName: 'Transaction mode',
-	}),
-
-	processorId: paymentProcessorIdSchema.optional(),
-
-	categoryId: categoryIdSchema,
-
-	// `From` can be different based on the transaction type
-	// - For INCOME, it should be either a merchant ID or an person ID
-	// - For Expense and Transfer, it should be an account ID
+	/**
+	 * If type is 'EXPENSE' | 'TRANSFER', from should be an account ID
+	 * If type is 'INCOME', from should be a merchant ID | person ID
+	 */
 	from: z
 		.object({
 			// This is used in a very specific case where the user wants to use money from another account to partially pay for the transaction. For example, lets say that the bill is 550rs and the user wants to pay through PhonePe. The user notices that they have 70rs in their PhonePe wallet. They can use this field to denote that the 70rs was used from the PhonePe wallet and the rest (480rs) was used from the account linked to the PhonePe.
@@ -83,15 +68,66 @@ const s2cTransactionSchemaRaw = z.object({
 		.or(merchantIdSchema)
 		.or(personIdSchema)
 		.or(accountIdSchema),
-	// `To` can be different based on the transaction type
-	// - For INCOME and Transfer, it should be an account ID
-	// - For Expense, it should be either a merchant ID or an person ID
+
+	/**
+	 * If type is 'EXPENSE' | 'TRANSFER', to should be an account ID
+	 * If type is 'INCOME', to should be a merchant ID | person ID
+	 */
 	to: accountIdSchema.or(merchantIdSchema).or(personIdSchema),
 
-	// Start date of the transaction. This is different from createdAt. createdAt is the date when the transaction was created. startDate is the date when the transaction should be considered for accounting purposes. For example, if a transaction is created on 1st Jan, but the startDate is 31st Dec, it means the transaction should be considered for accounting purposes from 31st Dec.
+	/**
+	 * Amount is sometimes auto filled based on the product selected. For example, if the user selects a product, the amount will be auto filled with the product amount. The user can change it if they want.
+	 */
+	amount: zodNumberSchema({
+		fieldName: 'Transaction amount',
+		min: TR.amount.MIN,
+		max: TR.amount.MAX,
+	}),
+
+	// status is defaulted to "COMPLETED" when the transaction is created. The user can change it to "PENDING" | "FAILED" if they want.
+	status: zodEnumSchema(TR.STATUSES, {
+		fieldName: 'Transaction status',
+	}),
+
+	/**
+	 * Mode is not defaulted to anything. The user has to select the mode. The mode is used to denote how the transaction was done. For example, if the user pays through UPI, the mode will be UPI. If the user pays through cash, the mode will be cash.
+	 */
+	mode: zodEnumSchema(TR.MODES, {
+		fieldName: 'Transaction mode',
+	}),
+
+	/**
+	 * If a product is selected, then the category will be auto filled based on the product's category. The user can't change it as a product can only belong to one category. (Disable the field if a product is selected)
+	 */
+	categoryId: categoryIdSchema,
+
+	/**
+	 * Payment processor is used to denote the payment processor used for the transaction. For example, if the user pays through PhonePe, the payment processor will be the PhonePe, if the user pays through Visa, the payment processor will be Visa, etc.
+	 * If this field is not present, it means the transaction was done without a payment processor. For example, if the user pays through cash, the payment processor will not be present.
+	 * This can also be auto filled based on the from field. If type is an expense and from is an account of type card, the payment processor will be auto filled with the card's payment processor.
+	 * If the type is an expense and mode is UPI, the payment processor will be auto filled with the primary UPI payment processor (CRED | GPay | PhonePe etc).
+	 * If the type is an expense and mode is NET_BANKING, the payment processor will be auto filled with the primary NET_BANKING payment processor (HDFC | ICICI | SBI etc).
+	 * If the user wants to change it, they can do so.
+	 */
+	processorId: paymentProcessorIdSchema.optional(),
+
+	/**
+	 * Transaction note can be used to store additional information about the transaction. Encourage the user to keep it short and sweet. Because unlike most apps currently in the market, we have dedicated fields for most of the things.
+	 * We even have a dedicated field for the split, which is not present in most apps.
+	 * We also have a dedicated field to neatly break down the transaction into multiple parts (This is useful when there are more than 1 product in the transaction)
+	 */
+	note: zodStringSchema({
+		fieldName: 'Transaction description',
+		maxLength: TR.description.MAX_LENGTH,
+	}).optional(),
+
+	/**
+	 * Start date of the transaction. This is different from createdAt. createdAt is the date when the transaction was created. startDate is the date when the transaction should be considered for accounting purposes. For example, if a transaction is created on 1st Jan, but the startDate is 31st Dec, it means the transaction should be considered for accounting purposes from 31st Dec.
+	 * This is defaulted to the current date. The user can change it if they want.
+	 */
 	startDate: zodDateSchema({
 		allowFuture: true,
-		fieldName: 'Start date',
+		fieldName: 'Transaction date',
 	}),
 
 	// Split details (split amounts, debt status like paid | paid_in_cash | pending, persons involved in the split, etc.)
@@ -109,6 +145,10 @@ const s2cTransactionSchemaRaw = z.object({
 					status: zodEnumSchema(TR.split.DEBT_STATUSES, {
 						fieldName: 'Debt status',
 					}),
+					/**
+					 * If a person involved in the split has settled the debt, the user can change the status to "SETTLED". The user will then be prompted to create the transaction for the settlement. This transaction is also added as a child transaction of the main transaction.
+					 */
+					transactionId: transactionIdSchema.optional(),
 				})
 			),
 		})
@@ -140,34 +180,52 @@ const s2cTransactionSchemaRaw = z.object({
 			breakup: z
 				.object({
 					list: z.array(
-						z.object({
-							name: zodStringSchema({
-								fieldName: 'Breakup name',
-								minLength: TR.merchant.breakup.list.name.MIN_LENGTH,
-								maxLength: TR.merchant.breakup.list.name.MAX_LENGTH,
-							}),
+						z
+							.object({
+								name: zodStringSchema({
+									fieldName: 'Product name',
+									minLength: TR.merchant.breakup.list.name.MIN_LENGTH,
+									maxLength: TR.merchant.breakup.list.name.MAX_LENGTH,
+								}).optional(),
 
-							quantity: zodNumberSchema({
-								fieldName: 'Quantity',
-								min: TR.merchant.breakup.list.quantity.MIN,
-							}),
+								productId: productIdSchema.optional(),
 
-							unit: zodEnumSchema(TR.merchant.breakup.list.unit.TYPES, {
-								fieldName: 'Unit',
-							}),
+								quantity: zodNumberSchema({
+									fieldName: 'Quantity',
+									min: TR.merchant.breakup.list.quantity.MIN,
+								}),
 
-							amountPerUnit: zodNumberSchema({
-								fieldName: 'Amount per unit',
-								min: TR.merchant.breakup.list.amountPerUnit.MIN,
-								max: TR.merchant.breakup.list.amountPerUnit.MAX,
-							}),
+								unit: zodEnumSchema(TR.merchant.breakup.list.unit.TYPES, {
+									fieldName: 'Unit',
+								}),
 
-							discount: zodNumberSchema({
-								fieldName: 'Discount',
-								min: TR.merchant.breakup.list.discount.MIN,
-								max: TR.merchant.breakup.list.discount.MAX,
-							}).optional(),
-						})
+								// amountPerUnit: zodNumberSchema({
+								// 	fieldName: 'Amount per unit',
+								// 	min: TR.merchant.breakup.list.amountPerUnit.MIN,
+								// 	max: TR.merchant.breakup.list.amountPerUnit.MAX,
+								// }),
+								subTotal: zodNumberSchema({
+									fieldName: 'Sub total',
+									min: TR.merchant.breakup.list.subTotal.MIN,
+									max: TR.merchant.breakup.list.subTotal.MAX,
+								}),
+
+								discount: zodNumberSchema({
+									fieldName: 'Discount',
+									min: TR.merchant.breakup.list.discount.MIN,
+									max: TR.merchant.breakup.list.discount.MAX,
+								}).optional(),
+							})
+							.refine(
+								(data) => {
+									// Name or productId should be present
+									return data.name || data.productId;
+								},
+								{
+									message:
+										TR.errors.merchant.breakup.list.NAME_OR_PRODUCT_REQUIRED,
+								}
+							)
 					),
 
 					// Metadata like tax, service charge, discount, etc.
@@ -201,7 +259,10 @@ const s2cTransactionSchemaRaw = z.object({
 		})
 		.optional(),
 
-	// Spent for is an optional field. It is used to denote if the transaction was done for someone else. This is not used if the user is expecting the money back. For that, we use the split field. This is used when the user pays for someone else and the user wants to keep track of it. For example, if the user pays for their sister's school fees, they can use this field to denote that the money was spent for their sister.
+	/**
+	 * Spent for is an optional field. It is used to denote if the transaction was done for someone else. This is not used if the user is expecting the money back. For that, we use the split field. This is used when the user pays for someone else and the user wants to keep track of it. For example, if the user pays for their sister's school fees, they can use this field to denote that the money was spent for their sister.
+	 * The ui to add this field will be shown only if the transaction type is "EXPENSE" and the user checks the "Spent for someone else" checkbox. If the user checks the checkbox, they will be prompted to add the person's ID for whom the transaction was done. They can also add additional people if the transaction was done for multiple people.
+	 */
 	spentFor: z
 		.object({
 			// Person ID
