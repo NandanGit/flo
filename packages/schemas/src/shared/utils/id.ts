c5f4069b79d7156c6ac05a46256c515486f2ee4f
Nandan Reddy <nandan.reddy1133@gmail.com>
2024-03-19 21:52:57 +0530

    Refactor resolveZodId function in id.ts

--- packages/schemas/src/shared/utils/id.ts
+++ packages/schemas/src/shared/utils/id.ts
@@ -1,26 +1,5 @@
-import { EntityIdPrefix, EntityType } from '../enums/entity';
+import { EntityType } from '../enums/entity';
 import { resolveEntityTypeFromId } from './entity';
-
-// export const resolveZodId = (id: string) => {
-// 	// The string above will be a valid zod id. We have to resolve it based on the prefix
-// 	const prefix = id.split('-')[0];
-// 	switch (prefix) {
-// 		case EntityIdPrefix.account:
-// 			return 'ACCOUNT';
-// 		case EntityIdPrefix.merchant:
-// 			return 'MERCHANT';
-// 		case EntityIdPrefix.person:
-// 			return 'PERSON';
-// 		case EntityIdPrefix.transaction:
-// 			return 'TRANSACTION';
-// 		case EntityIdPrefix.user:
-// 			return 'USER';
-// 		case EntityIdPrefix.category:
-// 			return 'CATEGORY';
-// 		default:
-// 			return 'UNKNOWN';
-// 	}
-// };
 
 export const resolveAccountType = (id: string) => {
 	return resolveEntityTypeFromId(id) as
